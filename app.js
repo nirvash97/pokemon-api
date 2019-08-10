acea226a829e55458fe69bd9ebaec8f4098835f7
Tossaporn Meesiri <59160273@go.buu.ac.th>
2019-08-10 12:05:52 +0700

    update app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+let pokemon = [
+    {name : 'pikachu' ,
+    type: 'Electri'},
+    {
+        name : 'T-rex' ,
+        type: 'rex',
+    }
+]
+app.get("/", (req, res) => res.send("Hello World! Pokemon"));
+app.get("/pokemons", (req, res) => res.send(pokemon));
+app.post("/pokemons",(req,res) => 
+{
+    pokemon.push(req.body)
+    res.sendStatus(201)
+})
+app.listen(port, () => console.log(`Pokemon-Api listening on port ${port}!`));
